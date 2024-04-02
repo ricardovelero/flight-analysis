1bf80e1ed5c3d13f544a14f2ea003f0be8bd1a19
Ricardo Rodriguez <ricardovelero@gmail.com>
2024-04-02 11:05:20 +0200

    Stuff

---
+++ src/google_flight_analysis/urls.py
@@ -0,0 +1,32 @@
+https://www.google.com/travel/flights?q=Flights%20to%20LIS%20from%20MAD
+
+https://www.google.com/travel/flights?q=Flights%20to%20SIN%20from%20MAD
+
+# Fin de semana desde Malaga
+https://www.google.com/travel/explore?tfs=CBwQAxoPag0IAhIJL20vMDE5NzhkGg9yDQgCEgkvbS8wMTk3OGRAAUgBcAKCAQ0I____________ARABmAEB&tfu=GgA&tcfs=UgRgAXgB
+
+# Una semana desde Malaga
+https://www.google.com/travel/explore?tfs=CBwQAxoPag0IAhIJL20vMDE5NzhkGg9yDQgCEgkvbS8wMTk3OGRAAUgBcAKCAQsI____________AZgBAQ&tfu=GgA&tcfs=UgRgAXgB
+
+# Una semana desde Sevilla
+https://www.google.com/travel/explore?tfs=CBwQAxoJagcIARIDU1ZRGglyBwgBEgNTVlFAAUgBcAKCAQsI____________AZgBAQ&tfu=GgA
+
+# Fin de semana desde Madrid:
+https://www.google.com/travel/explore?tfs=CBwQAxoJagcIARIDTUFEGglyBwgBEgNNQURAAUgBcAKCAQ0I____________ARABmAEB&tfu=GgA&tcfs=UgRgAXgB
+
+# Dos semanas desde Madrid:
+https://www.google.com/travel/explore?tfs=CBwQAxoJagcIARIDTUFEGglyBwgBEgNNQURAAUgBcAKCAQ0I____________ARADmAEB&tfu=GgA&tcfs=UgRgAXgB
+
+
+# Dos semanas desde Malaga:
+https://www.google.com/travel/explore?tfs=CBwQAxoPag0IAhIJL20vMDE5NzhkGg9yDQgCEgkvbS8wMTk3OGRAAUgBcAKCAQ0I____________ARADmAEB
+
+
+https://www.google.com/travel/explore?q=Flights%20from%20MAD&hl=es&gl=es&cur=eur
+
+https://www.google.com/travel/explore?tfs=Flights to CDG from MAD on weekends
+
+
+# Madrid a Paris
+
+https://www.google.com/travel/explore?tfs=CBwQAxoJagcIARIDTUFEGglyBwgBEgNNQURAAUgBcAKCAQsI____________AZgBAbIBEhgBIAEqDAgCEggvbS8wNXF0ag&tfu=GgA
